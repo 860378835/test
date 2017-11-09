2d174a2b2d670e13c7e862beb55313f3108e60e2
admin-PC\admin <860378835@qq.com>
2017-11-09 11:45:37 +0800

    添加Index

---
+++ web/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/11/9
+ * Time: 11:43
+ */
+
+namespace web\controller;
+
+
+class Index
+{
+    public function index(){
+        echo "hdphp";
+    }
+}
